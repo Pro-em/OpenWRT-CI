cff453fabb2d18efd22bf8fb0f11526573675fef
Pro-em <31509352+Pro-em@users.noreply.github.com>
2026-09-20 16:13:59 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -60,7 +60,7 @@ UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
-UPDATE_PACKAGE "vmlinux-btf" "kenzok8/vmlinux-btf" "master"
+UPDATE_PACKAGE "vmlinux-btf" "kenzok8/vmlinux-btf" "main"
 UPDATE_PACKAGE "daed" "corevie/luci-app-daed" "main"
 
 UPDATE_PACKAGE "diskmanager" "4IceG/luci-app-mini-diskmanager" "main"
